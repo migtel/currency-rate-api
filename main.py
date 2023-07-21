3c451174836425054e1a89ab9cfd4fc911a3cb21
Andres Linarez <andreslinarez9@gmail.com>
2023-07-21 15:12:29 -0400

    New feature: More currencies added

--- main.py
+++ main.py
@@ -5,25 +5,123 @@ from urllib3 import disable_warnings
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import requests
-from typing import Optional
 
 app = FastAPI()
-origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-@app.get("/dolar")
-async def get_dolar(dolar_type: Optional[str] = None):
+async def get_soup():
     disable_warnings(InsecureRequestWarning)
-    url = 'https://www.bcv.org.ve/'
+    url = "https://www.bcv.org.ve/"
     response = requests.get(url, verify=False)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    dolar = float(soup.find('div', {'id': 'dolar'}).text.strip().replace(',', '.').replace("USD \n ", ""))
+    soup = BeautifulSoup(response.text, "html.parser")
+    return soup
+
+
+@app.get("/")
+async def get_all():
+    soup = await get_soup()
+    dolar = float(
+        soup.find("div", {"id": "dolar"})
+        .text.strip()
+        .replace(",", ".")
+        .replace("USD \n ", "")
+    )
+    euro = float(
+        soup.find("div", {"id": "euro"})
+        .text.strip()
+        .replace(",", ".")
+        .replace("EUR  \n ", "")
+    )
+    yuan = float(
+        soup.find("div", {"id": "yuan"})
+        .text.strip()
+        .replace(",", ".")
+        .replace("CNY  \n ", "")
+    )
+    lira = float(
+        soup.find("div", {"id": "lira"})
+        .text.strip()
+        .replace(",", ".")
+        .replace("TRY \n ", "")
+    )
+    rublo = float(
+        soup.find("div", {"id": "rublo"})
+        .text.strip()
+        .replace(",", ".")
+        .replace("RUB \n ", "")
+    )
+    return {
+        "usd": dolar,
+        "eur": euro,
+        "cny": yuan,
+        "try": lira,
+        "rub": rublo
+    }
+
+
+@app.get("/usd")
+async def get_usd():
+    soup = await get_soup()
+    dolar = float(
+        soup.find("div", {"id": "dolar"})
+        .text.strip()
+        .replace(",", ".")
+        .replace("USD \n ", "")
+    )
     return dolar
+
+
+@app.get("/eur")
+async def get_eur():
+    soup = await get_soup()
+    euro = float(
+        soup.find("div", {"id": "euro"})
+        .text.strip()
+        .replace(",", ".")
+        .replace("EUR  \n ", "")
+    )
+    return euro
+
+
+@app.get("/cny")
+async def get_cny():
+    soup = await get_soup()
+    yuan = float(
+        soup.find("div", {"id": "yuan"})
+        .text.strip()
+        .replace(",", ".")
+        .replace("CNY  \n ", "")
+    )
+    return yuan
+
+
+@app.get("/try")
+async def get_try():
+    soup = await get_soup()
+    lira = float(
+        soup.find("div", {"id": "lira"})
+        .text.strip()
+        .replace(",", ".")
+        .replace("TRY \n ", "")
+    )
+    return lira
+
+
+@app.get("/rub")
+async def get_rub():
+    soup = await get_soup()
+    rublo = float(
+        soup.find("div", {"id": "rublo"})
+        .text.strip()
+        .replace(",", ".")
+        .replace("RUB \n ", "")
+    )
+    return rublo
